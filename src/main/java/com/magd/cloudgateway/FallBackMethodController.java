3546964bcd58362eee528e88f49015bf0472f744
ALex MAJIDI <mailofmajidi@gmail.com>
2022-01-29 19:54:09 +0000

    Commit a gateway service for MService proj , other services uploaded... Cloud system

---
+++ src/main/java/com/magd/cloudgateway/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package com.magd.cloudgateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try again later";
+    }
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try again later";
+    }
+}
